10e7162e4b258748b9d20312a829aa4a6e786016
belarif <b.ocine@live.fr>
2021-12-25 22:32:18 +0100

    doctrine fixture bundle installation

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
+
+
