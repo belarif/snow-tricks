2b86023808d962284040c6ca86502e2d9539314d
belarif <b.ocine@live.fr>
2021-12-27 14:29:51 +0100

    creation of route to homepage  with annotations

---
+++ src/Controller/FrontOffice/TrickController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\FrontOffice;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TrickRepository;
+
+class TrickController extends AbstractController
+{
+    /**
+     * @Route("/snow-tricks/accueil", name="homepage", methods={"GET"})
+     */
+
+}
+
+
