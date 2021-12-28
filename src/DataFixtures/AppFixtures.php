118dc5af3dce70ba2ce180ed285c4426dfbc0176
belarif <b.ocine@live.fr>
2021-12-28 19:09:15 +0100

    ST-21: insert tricks in database with fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\Group;
+use App\Entity\Trick;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
@@ -13,8 +16,30 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
+        /** Creation of tricks **/
+        $user_id = 1;
+        $userRepository = $manager->getRepository(User::class);
+        $user = $userRepository->find($user_id);
 
+        $group_id = 4;
+        $groupRepository = $manager->getRepository(Group::class);
+        $group = $groupRepository->find($group_id);
+
+        if ($user) {
+            if ($group) {
+                $trick = new Trick();
+                $trick->setName('Back flips');
+                $trick->setUser($user);
+                $trick->setDescription('Les back flips, rotations en arrière.
+                Il est possible de faire plusieurs flips à la suite, et d\'ajouter un grab à la rotation.');
+                $trick->setGroup($group);
+                $manager->persist($trick);
+                $manager->flush();
+            }
+        }
+        /** End creation of tricks **/
     }
 }
+
 
 
