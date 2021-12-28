d51d851f4f68e83c143730e2e0f4d2eb87a02e92
belarif <b.ocine@live.fr>
2021-12-28 19:30:09 +0100

    ST-10: adding prefix for all routes of TrickController

--- src/Controller/BackOffice/TrickController.php
+++ src/Controller/BackOffice/TrickController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/dashboard", name="admin_")
+ */
 class TrickController extends AbstractController
 {
 
