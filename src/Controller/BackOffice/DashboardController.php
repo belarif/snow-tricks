9fcaaa077489f8e9da01af9bc5704ae006fcee87
belarif <b.ocine@live.fr>
2021-12-28 15:10:12 +0100

    ST-21: creating route annotation

--- src/Controller/BackOffice/DashboardController.php
+++ src/Controller/BackOffice/DashboardController.php
@@ -3,8 +3,14 @@
 namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/snow-tricks" name="admin_")
+ */
 class DashboardController extends AbstractController
 {
-
+    /**
+     * @Route("/dashboard", name="dashboard", methods={"GET"})
+     */
 }
