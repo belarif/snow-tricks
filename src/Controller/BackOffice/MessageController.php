fdb7c3c6e28b24a862c94f1b0a6c4cfd7c8266b7
belarif <b.ocine@live.fr>
2021-12-29 02:51:27 +0100

    ST-13: adding ''/admin/messages' prefix for MessageController routes

--- src/Controller/BackOffice/MessageController.php
+++ src/Controller/BackOffice/MessageController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/messages", name="admin_")
+ */
 class MessageController extends AbstractController
 {
 
