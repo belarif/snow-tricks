209fa6a76807f3253d81db4bb006ee67eca6f5f3
belarif <b.ocine@live.fr>
2021-12-31 13:15:05 +0100

    ST-13: creating new message with fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\Message;
+use App\Entity\Trick;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
@@ -13,8 +16,21 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
+        /*** create message ***/
+        $user_id = 2;
+        $userRepository = $manager->getRepository(User::class);
+        $user = $userRepository->find($user_id);
 
+        $trick_id = 1;
+        $trickRepository = $manager->getRepository(Trick::class);
+        $trick = $trickRepository->find($trick_id);
+
+        $message = new Message();
+        $message->setTrick($trick);
+        $message->setUser($user);
+        $message->setContent('Ceci est le message test laisser par alex pour le trick mute de groupe grab');
+        $manager->persist($message);
+        $manager->flush();
+        /*** End create message ***/
     }
 }
-
-
