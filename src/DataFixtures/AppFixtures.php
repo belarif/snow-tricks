3cddeaad6ceaa0177a25642e1ebc1a122a915bcb
belarif <b.ocine@live.fr>
2022-01-03 10:55:11 +0100

    ST-2: adding types in type table with fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\Type;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
@@ -13,7 +14,15 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-
+        /*** Creation of types ***/
+        $types = ['image', 'video'];
+        foreach ($types as $newType) {
+            $type = new Type();
+            $type->setName($newType);
+            $manager->persist($type);
+            $manager->flush();
+        }
+        /*** End creation of types ***/
     }
 }
 
