2d8cce2623a7725b5f6e0821ed2bf5831ead7951
belarif <b.ocine@live.fr>
2022-01-03 22:09:56 +0100

    ST-2: creating images for trick-snow with fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Media;
+use App\Entity\Trick;
 use App\Entity\Type;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -23,6 +25,25 @@ class AppFixtures extends Fixture
             $manager->flush();
         }
         /*** End creation of types ***/
+
+        /** Creation of media **/
+        $trick_id = '6';
+        $trickRepository = $manager->getRepository(Trick::class);
+        $trick = $trickRepository->find($trick_id);
+
+        $type_id = 1;
+        $typeRepository = $manager->getRepository(Type::class);
+        $type = $typeRepository->find($type_id);
+
+        if ($trick) {
+            $media = new Media();
+            $media->setSrc('nose-slide.jpg');
+            $media->setTrick($trick);
+            $media->setType($type);
+            $manager->persist($media);
+            $manager->flush($media);
+        }
+        /** End creation of media **/
     }
 }
 
