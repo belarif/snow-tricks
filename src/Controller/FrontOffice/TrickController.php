18d386e006398c71cfd4a7511204460786c0a1ab
belarif <b.ocine@live.fr>
2022-01-05 02:43:37 +0100

    ST-3: creating function show that render trick details view

--- src/Controller/FrontOffice/TrickController.php
+++ src/Controller/FrontOffice/TrickController.php
@@ -13,4 +13,9 @@ class TrickController extends AbstractController
     /**
      * @Route("/details/{slug}", name="details")
      */
+    public function show()
+    {
+
+        return $this->render('/frontoffice/trick_details.html.twig');
+    }
 }
