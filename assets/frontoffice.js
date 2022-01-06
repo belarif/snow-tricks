1896907f791195a07312c81d966afb48f43acaf2
belarif <b.ocine@live.fr>
2022-01-06 15:40:24 +0100

    ST-3: call library jquery

--- assets/frontoffice.js
+++ assets/frontoffice.js
@@ -1,2 +1,10 @@
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
 import('./styles/frontoffice/styles.css');
 import('./styles/frontoffice/home.css');
+import('./styles/frontoffice/trick_details.css');
+import('./js/frontoffice/trick_details');
