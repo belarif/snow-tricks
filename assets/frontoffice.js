69df27f16a1e35d2b8c100811af979f65ffba686
belarif <b.ocine@live.fr>
2022-01-07 16:26:56 +0100

    ST-23: importing edit_trick css file

--- assets/frontoffice.js
+++ assets/frontoffice.js
@@ -7,4 +7,5 @@ global.$ = global.jQuery = $;
 import('./styles/frontoffice/styles.css');
 import('./styles/frontoffice/home.css');
 import('./styles/frontoffice/trick_details.css');
+import('./styles/frontoffice/edit_trick.css');
 import('./js/frontoffice/trick_details');
