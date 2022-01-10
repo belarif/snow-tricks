b76e7eb6bb70fb696b4eb1082ab1526720b7a13f
belarif <b.ocine@live.fr>
2022-01-10 19:44:51 +0100

    ST-22: changing type of medias attribut

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -6,6 +6,7 @@ use App\Entity\Group;
 use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,12 +19,16 @@ class TrickType extends AbstractType
             ->add('description')
             ->add('group', EntityType::class, [
                 'class' => Group::class,
-                'placeholder' => 'choisir un groupe',
+                'placeholder' => 'Choisir un groupe',
                 'choice_label' => function (Group $Group) {
                     return $Group->getName();
                 }, 'choice_value' => function ($Group) {
                     return $Group ? $Group->getId() : '';
-                }]);
+                }])
+            ->add('medias', FileType::class, [
+                'data_class' => null,
+                'multiple' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
