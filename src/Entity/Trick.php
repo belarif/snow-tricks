0d9a9c6bec5ac23c5ea38f2e104b06a631c31c8e
belarif <b.ocine@live.fr>
2022-01-10 21:06:40 +0100

    ST-22: apply the uniqueness constraint on the name attribute of the entity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -7,11 +7,13 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
  * @ORM\Table(name="st_trick")
+ * @UniqueEntity("name")
  */
 class Trick
 {
