e7112883a3e6bd7126b11b283c24491a7b33aa27
belarif <b.ocine@live.fr>
2022-01-10 13:19:30 +0100

    ST-22: importing add_trick.html.twig  file

--- assets/frontoffice.js
+++ assets/frontoffice.js
@@ -9,3 +9,4 @@ import('./styles/frontoffice/home.css');
 import('./styles/frontoffice/trick_details.css');
 import('./styles/frontoffice/edit_trick.css');
 import('./js/frontoffice/trick_details');
+import('./styles/frontoffice/add_trick.css');
