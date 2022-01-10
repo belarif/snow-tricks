f9fb07c9d82102c39212507a3ef7cb64f2a3d2bd
belarif <b.ocine@live.fr>
2022-01-10 23:30:51 +0100

    ST-22: creation of images and videos of trick

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,36 +16,6 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        /*** Creation of types ***/
-        /*$types = ['image', 'video'];
-        foreach ($types as $newType) {
-            $type = new Type();
-            $type->setName($newType);
-            $manager->persist($type);
-            $manager->flush();
-        }*/
-        /*** End creation of types ***/
-
-        /** Creation of media **/
-        /*$trick_id = '1';
-        $trickRepository = $manager->getRepository(Trick::class);
-        $trick = $trickRepository->find($trick_id);
-
-        $type_id = 1;
-        $typeRepository = $manager->getRepository(Type::class);
-        $type = $typeRepository->find($type_id);
-
-        if ($trick) {
-            $media = new Media();
-            $media->setSrc('mute-grab-4.jpg');
-            $media->setTrick($trick);
-            $media->setType($type);
-            $manager->persist($media);
-            $manager->flush($media);
-        }*/
-        /** End creation of media **/
-
-
         /** Creation of videos and images **/
         $trick_id = '1';
         $trickRepository = $manager->getRepository(Trick::class);
