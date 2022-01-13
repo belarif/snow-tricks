c2ed10f801653a31bbc76c0af33260a991a8ea1c
belarif <b.ocine@live.fr>
2022-01-13 18:28:52 +0100

    ST-4: creating register function that renderForm registerType

--- src/Controller/FrontOffice/UserController.php
+++ src/Controller/FrontOffice/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\FrontOffice;
 
+use App\Entity\User;
+use App\Form\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,4 +12,10 @@ class UserController extends AbstractController
     /**
      * @Route("/registration", name="user_registration")
      */
+    public function register()
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterFormType::class, $user);
+        return $this->renderForm('/frontoffice/registration.html.twig', array('form' => $form));
+    }
 }
