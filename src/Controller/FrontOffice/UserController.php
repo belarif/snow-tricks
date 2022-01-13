931f4cc341a46fcce4d931e2e13a99c8e3500f76
belarif <b.ocine@live.fr>
2022-01-13 16:22:32 +0100

    ST-4: creating route to register function

--- src/Controller/FrontOffice/UserController.php
+++ src/Controller/FrontOffice/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\FrontOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-
+    /**
+     * @Route("/registration", name="user_registration")
+     */
 }
