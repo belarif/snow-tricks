c1f4ece2551449e64e5612f0a41215d0ef539743
belarif <b.ocine@live.fr>
2022-01-13 19:16:55 +0100

    ST-4: adding of form processing

--- src/Controller/FrontOffice/UserController.php
+++ src/Controller/FrontOffice/UserController.php
@@ -4,7 +4,10 @@ namespace App\Controller\FrontOffice;
 
 use App\Entity\User;
 use App\Form\RegisterFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -12,10 +15,20 @@ class UserController extends AbstractController
     /**
      * @Route("/registration", name="user_registration")
      */
-    public function register()
+    public function register(Request $request, ManagerRegistry $doctrine): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $em = $doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('user_registration');
+        }
         return $this->renderForm('/frontoffice/registration.html.twig', array('form' => $form));
     }
 }
