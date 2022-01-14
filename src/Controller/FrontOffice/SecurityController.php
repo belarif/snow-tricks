f8365cc511f724263a8b4126a289bbad5b769e55
belarif <b.ocine@live.fr>
2022-01-14 18:10:14 +0100

    ST-5: creating route to login function

--- src/Controller/FrontOffice/SecurityController.php
+++ src/Controller/FrontOffice/SecurityController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\FrontOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/login", name="user_login")
+     */
 
 }
