561b36ab0c7b4f28234a87ef5a9bee0d201e080d
belarif <b.ocine@live.fr>
2022-01-14 12:55:01 +0100

    ST-4: modifying parameter getUserIdentifier function ans deleting getUsername() function

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -108,15 +108,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string)$this->email;
-    }
-
-    /**
-     * @deprecated since Symfony 5.3, use getUserIdentifier instead
-     */
-    public function getUsername(): string
-    {
-        return (string)$this->email;
+        return (string)$this->username;
     }
 
     /**
