8887c122c4aacf88dc088b74faecb47f06df5d7e
belarif <b.ocine@live.fr>
2022-01-14 19:50:25 +0100

    ST-5: using AuthenticationUtils class to manage authentication

--- src/Controller/FrontOffice/SecurityController.php
+++ src/Controller/FrontOffice/SecurityController.php
@@ -3,15 +3,24 @@
 namespace App\Controller\FrontOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="user_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('/frontoffice/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('/frontoffice/login.html.twig', [
+            'controller_name' => 'SecurityController',
+            'lastUsername' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 }
