9cf62f9995cc5ed6ad13bd9d51cd94e0cbf1010e
belarif <b.ocine@live.fr>
2022-01-17 22:01:44 +0100

    ST-24: creating route to profile function

--- src/Controller/FrontOffice/ProfileController.php
+++ src/Controller/FrontOffice/ProfileController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\FrontOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-
+    /**
+     * @Route("/profile", name="user_profile")
+     */
 }
