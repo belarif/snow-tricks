19836a41503593ecd18a4ccc9c9978c7200814b7
belarif <b.ocine@live.fr>
2022-01-17 23:01:12 +0100

    ST-24: creating profileFormType

---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('firstName', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('photo', FileType::class, [
+                'attr' => ['class' => 'form-control'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
