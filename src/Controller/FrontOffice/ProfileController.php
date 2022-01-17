bdad0d9384fa132ce64f0acf99de7c32602c4d65
belarif <b.ocine@live.fr>
2022-01-17 23:00:51 +0100

    ST-24: creating profile function that manage profile view

--- src/Controller/FrontOffice/ProfileController.php
+++ src/Controller/FrontOffice/ProfileController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller\FrontOffice;
 
+use App\Entity\User;
+use App\Form\ProfileFormType;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -10,4 +16,17 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile", name="user_profile")
      */
+    public function profile(UserRepository $userRepository, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $user = new User();
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $user = $form->getData();
+            $em->persist($user);
+            $em->flush();
+        }
+        return $this->renderForm('/frontoffice/profile.html.twig', array('form' => $form));
+    }
 }
