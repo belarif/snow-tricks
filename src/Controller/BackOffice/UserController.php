048a8790b74fb98a95d65fe41b82ae75b7557b5a
belarif <b.ocine@live.fr>
2022-01-22 18:08:02 +0100

    ST-17: creating show user detail function

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller\BackOffice;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,10 +20,21 @@ class UserController extends AbstractController
     public function usersList(UserRepository $userRepository)
     {
         $users = $userRepository->getUsers();
-        return $this->render('/backoffice/usersList.html.twig', ['users' => $users]);
+        return $this->render('/backoffice/usersList.html.twig', [
+            'users' => $users
+        ]);
     }
 
     /**
-     * @Route("/details", name="user_details")
+     * @Route("/details/{slug}", name="user_details")
      */
+    public function show(userRepository $userRepository, Request $request): Response
+    {
+        $slug = $request->get('slug');
+        $userDetails = $userRepository->getUser($slug);
+        return $this->render(
+            '/backoffice/userDetails.html.twig', [
+            'userDetails' => $userDetails
+        ]);
+    }
 }
