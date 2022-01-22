2880cb4c5c5c0757fa639c7478f2f4e6095b8a0a
belarif <b.ocine@live.fr>
2022-01-22 07:57:39 +0100

    ST-16: creating route to users list and route prefix on UserController class

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/users", name="admin_")
+ *
+ */
 class UserController extends AbstractController
 {
-
+    /**
+     * @Route("/list", name="users_list")
+     */
 }
