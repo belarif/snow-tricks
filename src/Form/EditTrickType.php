673ea16c32664bc010347dcf84cd3c7ef797baaa
belarif <b.ocine@live.fr>
2022-01-27 17:33:17 +0100

    ST-26: customizing object form

--- src/Form/EditTrickType.php
+++ src/Form/EditTrickType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Group;
 use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +16,22 @@ class EditTrickType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('updatedAt')
-            ->add('createdAt')
-            ->add('slug')
-            ->add('group')
-            ->add('user');
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('group', EntityType::class, [
+                'class' => Group::class,
+                'placeholder' => 'Choisir un groupe',
+                'label' => 'Groupe',
+                'choice_label' => function (Group $Group) {
+                    return $Group->getName();
+                }, 'choice_value' => function ($Group) {
+                    return $Group ? $Group->getId() : '';
+                }
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
