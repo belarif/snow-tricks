7dcb71bcb6e794c0a6d77a1ac37dd8c16ed3920b
belarif <b.ocine@live.fr>
2022-01-28 12:32:08 +0100

    main: code reorganization and optimization

--- src/Controller/BackOffice/TrickController.php
+++ src/Controller/BackOffice/TrickController.php
@@ -5,6 +5,7 @@ namespace App\Controller\BackOffice;
 use App\Repository\TrickRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,37 +16,52 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TrickController extends AbstractController
 {
+    private $trickRepository;
+    private $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry, TrickRepository $trickRepository)
+    {
+        $this->managerRegistry = $managerRegistry;
+        $this->trickRepository = $trickRepository;
+    }
+
     /**
      * @Route("/list", name="tricks_list", methods={"GET"})
+     * @return Response
      */
-    public function tricksList(TrickRepository $trickRepository): Response
+    public function tricksList(): Response
     {
-        $tricks = $trickRepository->getTricks();
-        return $this->render('/backoffice/tricksList.html.twig', array('tricks' => $tricks));
+        $tricks = $this->trickRepository->getTricks();
+        return $this->render('/backoffice/tricksList.html.twig', ['tricks' => $tricks]);
     }
 
     /**
      * @Route("/details/{id}/{slug}", name="trick_details", methods={"GET"})
+     * @param Request $request
+     * @return Response
      */
-    public function show(TrickRepository $trickRepository, Request $request): Response
+    public function show(Request $request): Response
     {
         $id = $request->get('id');
-        $trickDetails = $trickRepository->getTrick($id);
-        return $this->render('/backoffice/trickDetails.html.twig', array('trickDetails' => $trickDetails));
+        $trickDetails = $this->trickRepository->getTrick($id);
+        return $this->render('/backoffice/trickDetails.html.twig', ['trickDetails' => $trickDetails]);
     }
 
     /**
      * @Route("/delete/{id}", name="trick_delete")
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function delete(TrickRepository $trickRepository, Request $request, ManagerRegistry $doctrine): Response
+    public function delete(Request $request): RedirectResponse
     {
         $trick_id = $request->get('id');
-        $trickDelete = $trickRepository->find($trick_id);
-        $em = $doctrine->getManager();
+        $trickDelete = $this->trickRepository->find($trick_id);
+        $em = $this->managerRegistry->getManager();
         $em->remove($trickDelete);
         $em->flush();
         return $this->redirectToRoute('admin_tricks_list');
     }
 }
+
 
 
