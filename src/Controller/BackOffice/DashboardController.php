9c09f3319fed4d8d6bead6e3707f947112445d48
belarif <b.ocine@live.fr>
2022-01-28 12:34:44 +0100

    main: adding return hint

--- src/Controller/BackOffice/DashboardController.php
+++ src/Controller/BackOffice/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,8 +14,9 @@ class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="dashboard", methods={"GET"})
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('backoffice/dashboard.html.twig');
     }
