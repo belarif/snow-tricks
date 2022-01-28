1908a725c7ee0d4202a3b9e2db5bfd923f91e8b8
belarif <b.ocine@live.fr>
2022-01-28 18:26:08 +0100

    main: adding enabled parameter to renderForm method

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -141,24 +141,25 @@ class UserController extends AbstractController
      */
     public function edit(Request $request): Response
     {
+        $id = $request->get('id');
+        $selectedUser = $this->userRepository->findOneBy(['id' => $id]);
+        $enabled = $selectedUser->getEnabled();
         $user = new User();
         $form = $this->createForm(EditUserType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $id = $request->get('id');
-            $this->update($id, $form);
+            $this->update($form, $selectedUser);
             return $this->redirectToRoute('admin_users_list');
         }
-        return $this->renderForm('/backoffice/userEdit.html.twig', ['form' => $form]);
+        return $this->renderForm('/backoffice/userEdit.html.twig', ['form' => $form, 'enabled' => $enabled]);
     }
 
     /**
      * @param $form
      * @param $id
      */
-    private function update($id, $form): void
+    private function update($form, $selectedUser): void
     {
-        $selectedUser = $this->userRepository->findOneBy(['id' => $id]);
         if (!$selectedUser) {
             throw $this->createNotFoundException('Utilisateur inexistant');
         }
