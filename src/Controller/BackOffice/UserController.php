dedcc8282f3fcface2ddcea2bf48e14a604356a3
belarif <b.ocine@live.fr>
2022-01-28 13:19:51 +0100

    main: code reorganization and optimization

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -10,6 +10,7 @@ use App\Repository\UserRepository;
 use App\Service\Mailer;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -22,38 +23,62 @@ use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
  */
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/list", name="users_list", methods={"GET"})
-     */
-    public function usersList(UserRepository $userRepository)
+    private $userRepository;
+    private $managerRegistry;
+    private $roleRepository;
+    private $passwordHasher;
+    private $tokenGenerator;
+    private $mailer;
+
+    public function __construct(
+        UserRepository              $userRepository,
+        ManagerRegistry             $managerRegistry,
+        RoleRepository              $roleRepository,
+        UserPasswordHasherInterface $passwordHasher,
+        TokenGeneratorInterface     $tokenGenerator,
+        Mailer                      $mailer
+    )
     {
-        $users = $userRepository->getUsers();
-        return $this->render('/backoffice/usersList.html.twig', [
-            'users' => $users
-        ]);
+        $this->userRepository = $userRepository;
+        $this->managerRegistry = $managerRegistry;
+        $this->roleRepository = $roleRepository;
+        $this->passwordHasher = $passwordHasher;
+        $this->tokenGenerator = $tokenGenerator;
+        $this->mailer = $mailer;
     }
 
     /**
-     * @Route("/details/{id}/{slug?}", name="user_details, methods={"GET"}")
+     * @Route("/list", name="users_list", methods={"GET"})
+     * @return Response
      */
-    public function show(userRepository $userRepository, Request $request): Response
+    public function usersList(): Response
+    {
+        $users = $this->userRepository->getUsers();
+        return $this->render('/backoffice/usersList.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/details/{id}/{slug?}", name="user_details", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function show(Request $request): Response
     {
         $id = $request->get('id');
-        $userDetails = $userRepository->getUser($id);
-        return $this->render(
-            '/backoffice/userDetails.html.twig', [
-            'userDetails' => $userDetails
-        ]);
+        $userDetails = $this->userRepository->getUser($id);
+        return $this->render('/backoffice/userDetails.html.twig', ['userDetails' => $userDetails]);
     }
 
     /**
      * @Route("/delete/{id}", name="user_delete")
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function delete(UserRepository $userRepository, Request $request, ManagerRegistry $doctrine): Response
+    public function delete(Request $request): redirectResponse
     {
         $user_id = $request->get('id');
-        $userDelete = $userRepository->find($user_id);
-        $em = $doctrine->getManager();
+        $userDelete = $this->userRepository->find($user_id);
+        $em = $this->managerRegistry->getManager();
         $em->remove($userDelete);
         $em->flush();
         return $this->redirectToRoute('admin_users_list');
@@ -61,42 +86,26 @@ class UserController extends AbstractController
 
     /**
      * @Route("/create", name="user_create", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
      */
-    public function new(
-        Request                     $request,
-        UserRepository              $userRepository,
-        RoleRepository              $roleRepository,
-        ManagerRegistry             $doctrine,
-        UserPasswordHasherInterface $passwordHasher,
-        TokenGeneratorInterface     $tokenGenerator,
-        Mailer                      $mailer
-    ): Response
+    public function new(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(CreateUserType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $form->get('email')->getData();
-            $existing_user = $userRepository->findOneBy(array('email' => $email));
+            $existing_user = $this->userRepository->findOneBy(array('email' => $email));
             if (!$existing_user) {
-                $password = $form->get('password')->getData();
-                $user->setPassword($passwordHasher->hashPassword($user, $password));
-                $role = $roleRepository->find('1');
-                $user->addRole($role);
-                $user->setRoles((array)$role->getRoleName());
-                $user->setToken($tokenGenerator->generateToken());
-                $user->setProfileStatus(false);
-
-                $em = $doctrine->getManager();
-                $em->persist($user);
-                $em->flush();
+                $this->processCreation($form, $user);
 
                 $email = $user->getEmail();
                 $username = $user->getUserIdentifier();
                 $token = $user->getToken();
                 $subject = 'activer votre compte SnowTricks';
                 $htmlTemplate = '/emails/activation.html.twig';
-                $mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
+                $this->mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
 
                 return $this->redirectToRoute('admin_users_list');
             } else {
@@ -108,30 +117,54 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/edit/{id}/{slug?}", name="user_edit", , methods={"PUT"})
+     * @param $form
+     * @param $user
      */
-    public function edit(
-        Request         $request,
-        UserRepository  $userRepository,
-        ManagerRegistry $doctrine
-    ): Response
+    private function processCreation($form, $user): void
+    {
+        $password = $form->get('password')->getData();
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+        $role = $this->roleRepository->find('1');
+        $user->addRole($role);
+        $user->setRoles((array)$role->getRoleName());
+        $user->setToken($this->tokenGenerator->generateToken());
+        $user->setProfileStatus(false);
+        $em = $this->managerRegistry->getManager();
+        $em->persist($user);
+        $em->flush();
+    }
+
+    /**
+     * @Route("/edit/{id}/{slug?}", name="user_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(EditUserType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $id = $request->get('id');
-            $selectedUser = $userRepository->findOneBy(['id' => $id]);
-            if (!$selectedUser) {
-                throw $this->createNotFoundException('Utilisateur inexistant');
-            }
-            $selectedUser->setEnabled($form->get('enabled')->getData());
-            $em = $doctrine->getManager();
-            $em->persist($selectedUser);
-            $em->flush();
+            $this->update($id, $form);
             return $this->redirectToRoute('admin_users_list');
         }
-
         return $this->renderForm('/backoffice/userEdit.html.twig', ['form' => $form]);
+    }
+
+    /**
+     * @param $form
+     * @param $id
+     */
+    private function update($id, $form): void
+    {
+        $selectedUser = $this->userRepository->findOneBy(['id' => $id]);
+        if (!$selectedUser) {
+            throw $this->createNotFoundException('Utilisateur inexistant');
+        }
+        $selectedUser->setEnabled($form->get('enabled')->getData());
+        $em = $this->managerRegistry->getManager();
+        $em->persist($selectedUser);
+        $em->flush();
     }
 }
