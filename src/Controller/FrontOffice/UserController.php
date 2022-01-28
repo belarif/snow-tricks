7ccb76eebb8835e67058e2e2b1bc91a5e2f692d1
belarif <b.ocine@live.fr>
2022-01-28 15:14:01 +0100

    main: code reorganization and optimization

--- src/Controller/FrontOffice/UserController.php
+++ src/Controller/FrontOffice/UserController.php
@@ -11,6 +11,7 @@ use App\Repository\UserRepository;
 use App\Service\Mailer;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,18 +20,37 @@ use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class UserController extends AbstractController
 {
+    private $managerRegistry;
+    private $roleRepository;
+    private $userRepository;
+    private $tokenGenerator;
+    private $passwordHasher;
+    private $mailer;
+
+    public function __construct(
+        ManagerRegistry             $managerRegistry,
+        RoleRepository              $roleRepository,
+        UserRepository              $userRepository,
+        TokenGeneratorInterface     $tokenGenerator,
+        UserPasswordHasherInterface $passwordHasher,
+        Mailer                      $mailer
+    )
+    {
+        $this->managerRegistry = $managerRegistry;
+        $this->roleRepository = $roleRepository;
+        $this->userRepository = $userRepository;
+        $this->tokenGenerator = $tokenGenerator;
+        $this->passwordHasher = $passwordHasher;
+        $this->mailer = $mailer;
+    }
+
     /**
      * @Route("/registration", name="app_registration")
+     *
+     * @param Request $request
+     * @return Response
      */
-    public function register(
-        Request                     $request,
-        ManagerRegistry             $doctrine,
-        UserPasswordHasherInterface $passwordHasher,
-        RoleRepository              $roleRepository,
-        Mailer                      $mailer,
-        TokenGeneratorInterface     $tokenGenerator,
-        UserRepository              $userRepository
-    ): Response
+    public function register(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterFormType::class, $user);
@@ -38,17 +58,15 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $form->get('email')->getData();
-            $existing_user = $userRepository->findOneBy(array('email' => $email));
+            $existing_user = $this->userRepository->findOneBy(['email' => $email]);
             if (!$existing_user) {
-                $password = $form->get('password')->getData();
-                $user->setPassword($passwordHasher->hashPassword($user, $password));
-                $role = $roleRepository->find('1');
+                $user->setPassword($this->passwordHasher->hashPassword($user, $form->get('password')->getData()));
+                $role = $this->roleRepository->find('1');
                 $user->addRole($role);
                 $user->setRoles((array)$role->getRoleName());
-                $user->setToken($tokenGenerator->generateToken());
+                $user->setToken($this->tokenGenerator->generateToken());
                 $user->setProfileStatus(false);
-
-                $em = $doctrine->getManager();
+                $em = $this->managerRegistry->getManager();
                 $em->persist($user);
                 $em->flush();
 
@@ -57,8 +75,7 @@ class UserController extends AbstractController
                 $token = $user->getToken();
                 $subject = 'activer votre compte SnowTricks';
                 $htmlTemplate = '/emails/activation.html.twig';
-                $mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
-
+                $this->mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
                 $this->addFlash(
                     'success',
                     'Votre compte a été créé avec succès, un mail d\'activation vous a été envoyé à l\'adresse : ' . $email
@@ -69,40 +86,44 @@ class UserController extends AbstractController
                 return $this->redirectToRoute('app_registration');
             }
         }
-        return $this->renderForm('/frontoffice/registration.html.twig', array('form' => $form));
+        return $this->renderForm('/frontoffice/registration.html.twig', ['form' => $form]);
     }
 
     /**
      * @Route("/confirm_account/{token}", name="app_account_confirmation")
+     *
+     * @param $token
+     * @return RedirectResponse
      */
-    public function confirmAccount($token, UserRepository $userRepository, ManagerRegistry $doctrine)
+    public function confirmAccount($token): redirectResponse
     {
-        $user = $userRepository->findOneBy(['token' => $token]);
+        $user = $this->userRepository->findOneBy(['token' => $token]);
         if ($user) {
             $user->setToken(null);
             $user->setEnabled(true);
-            $em = $doctrine->getManager();
+            $em = $this->managerRegistry->getManager();
             $em->persist($user);
             $em->flush();
-
             return $this->redirectToRoute('app_login');
         } else {
-
             return $this->redirectToRoute('app_homepage');
         }
     }
 
     /**
      * @Route("/forgot_password", name="app_forgot_password")
+     *
+     * @param Request $request
+     * @return Response
      */
-    public function forgotPassword(Request $request, UserRepository $userRepository, Mailer $mailer): Response
+    public function forgotPassword(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(ForgotPasswordFormType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $username = $form->get('username')->getData();
-            $user = $userRepository->findOneBy(array('username' => $username));
+            $user = $this->userRepository->findOneBy(['username' => $username]);
             if (!$user) {
                 $this->addFlash(
                     'existingUser',
@@ -110,21 +131,22 @@ class UserController extends AbstractController
                 );
                 return $this->redirectToRoute('app_forgot_password');
             }
-
             $email = $user->getEmail();
             $token = $user->getToken();
             $subject = 'Réinitialiser votre mot de passe';
             $htmlTemplate = '/emails/forgotPassword.html.twig';
-            $mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
+            $this->mailer->sendEmail($email, $username, $token, $subject, $htmlTemplate);
         }
-
         return $this->renderForm('frontoffice/forgotPassword.html.twig', ['form' => $form]);
     }
 
     /**
      * @Route("reset_password", name="app_reset_password")
+     *
+     * @param Request $request
+     * @return Response
      */
-    public function resetPassword(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher)
+    public function resetPassword(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(ResetPasswordFormType::class, $user);
@@ -133,12 +155,11 @@ class UserController extends AbstractController
             $username = $form->get('username')->getData();
             $password = $form->get('password')->getData();
             $user->setUsername($username);
-            $user->setPassword($passwordHasher->hashPassword($user, $password));
+            $user->setPassword($this->passwordHasher->hashPassword($user, $password));
 
-            $em = $doctrine->getManager();
+            $em = $this->managerRegistry->getManager();
             $em->persist($user);
             $em->flush();
-
             return $this->redirectToRoute('app_homepage');
         }
         return $this->renderForm('frontoffice/resetPassword.html.twig', ['form' => $form]);
