9724f944119e5828d964e82a2ce4376020394044
belarif <b.ocine@live.fr>
2022-01-28 13:30:01 +0100

    main: code reorganization and optimization and adding parameters annotations for index function

--- src/Controller/FrontOffice/HomeController.php
+++ src/Controller/FrontOffice/HomeController.php
@@ -4,17 +4,21 @@ namespace App\Controller\FrontOffice;
 
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="app_homepage")
+     * @Route("/accueil", name="app_homepage", methods={"GET"})
+     *
+     * @param TrickRepository $trickRepository
+     * @return Response
      */
-    public function index(TrickRepository $trickRepository)
+    public function index(TrickRepository $trickRepository): Response
     {
         $listTricks = $trickRepository->getTricks();
-        return $this->render('/frontoffice/home.html.twig', array('listTricks' => $listTricks));
+        return $this->render('/frontoffice/home.html.twig', ['listTricks' => $listTricks]);
     }
 }
 
