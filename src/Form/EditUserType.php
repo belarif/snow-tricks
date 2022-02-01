73f90e8b8fe47c92fd39f4d892b8002a1d680f12
belarif <b.ocine@live.fr>
2022-02-01 19:05:11 +0100

    ST-27: adding role field to edit roles user

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Role;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +18,14 @@ class EditUserType extends AbstractType
             ->add('enabled', CheckboxType::class, [
                 'attr' => ['class' => 'form-control'],
                 'required' => false,
-            ]);
+            ])
+            ->add('role', EntityType::class, [
+                    'class' => Role::class,
+                    'multiple' => true,
+                    'choice_label' => function (Role $Role) {
+                        return $Role->getRole();
+                    }]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
