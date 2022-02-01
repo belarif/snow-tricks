cdcf0fa40513db2d5f5a94a353ecfe9b36c6b5f2
belarif <b.ocine@live.fr>
2022-02-01 17:43:42 +0100

    ST-27: changing attribute that function getRoles return

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -146,7 +146,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->role->map(function (Role $role) {
-            return $role->getRole();
+            return $role->getRoleName();
         });
 
         return array_unique($roles->toArray());
