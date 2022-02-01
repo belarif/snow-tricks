411507ea60bb331e5db45c9f56ccffea9368e4a2
belarif <b.ocine@live.fr>
2022-02-01 17:46:58 +0100

    ST-27: limit access to admin dashboard

--- src/Controller/BackOffice/DashboardController.php
+++ src/Controller/BackOffice/DashboardController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller\BackOffice;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/admin", name="admin_")
- *
+ * @IsGranted("ROLE_ADMIN")
  */
 class DashboardController extends AbstractController
 {
