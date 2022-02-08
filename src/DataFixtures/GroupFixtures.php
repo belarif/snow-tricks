14bbd9ac436433f5fe65624120386a41a6481df9
belarif <b.ocine@live.fr>
2022-02-08 14:13:57 +0100

    ST-main: changing groups constants values

--- src/DataFixtures/GroupFixtures.php
+++ src/DataFixtures/GroupFixtures.php
@@ -8,12 +8,14 @@ use Doctrine\Persistence\ObjectManager;
 
 class GroupFixtures extends Fixture
 {
-	public const GROUP_ONE = 'GROUP_ONE';
-	public const GROUP_TWO = 'GROUP_TWO';
-	public const GROUP_THREE = 'GROUP_THREE';
-	public const GROUP_FOUR = 'GROUP_FOUR';
-	public const GROUP_FIVE = 'GROUP_FIVE';
-	public const GROUP_SIX = 'GROUP_SIX';
+    public const GROUP_ONE = 'Les grabs';
+    public const GROUP_TWO = 'La manière de rideur';
+    public const GROUP_THREE = 'Les rotations';
+    public const GROUP_FOUR = 'Les flips';
+    public const GROUP_FIVE = 'Les rotations désaxées';
+    public const GROUP_SIX = 'Les slides';
+    public const GROUP_SEVEN = 'Les one foot tricks';
+    public const GROUP_EIGHT = 'Old school';
 
     /**
      * @inheritDoc
@@ -21,12 +23,14 @@ class GroupFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $groups = [
-			self::GROUP_ONE,
-	        self::GROUP_TWO,
-			self::GROUP_THREE,
-			self::GROUP_FOUR,
-			self::GROUP_FIVE,
-			self::GROUP_SIX,
+            self::GROUP_ONE,
+            self::GROUP_TWO,
+            self::GROUP_THREE,
+            self::GROUP_FOUR,
+            self::GROUP_FIVE,
+            self::GROUP_SIX,
+            self::GROUP_SEVEN,
+            self::GROUP_EIGHT,
         ];
 
         foreach ($groups as $name) {
