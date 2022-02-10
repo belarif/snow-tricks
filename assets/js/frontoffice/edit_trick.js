7450fc045b1958ef2a9a0f962984feb856b58f5e
belarif <b.ocine@live.fr>
2022-02-10 17:59:45 +0100

    ST-33: creating deleteImage function js that redirect to deleteImage function in controller

--- assets/js/frontoffice/edit_trick.js
+++ assets/js/frontoffice/edit_trick.js
@@ -3,3 +3,14 @@ $(document).ready(function () {
         $("#edit_trick_images").show();
     });
 });
+
+function deleteImage(id) {
+    if (confirm("Êtes vous sûr de vouloir supprimer l'image ?")) {
+        window.location.href = "/snow-tricks/medias/image/delete/" + id;
+    } else {
+        alert("Vous avez annulé la suppression");
+    }
+}
+
+window.deleteImage = deleteImage;
+
