45f645790d79d1ec4e99d767a474f729b14bef0b
belarif <b.ocine@live.fr>
2022-02-15 15:43:55 +0100

    ST-35: creating js code that only display 15 tricks and display more onclick on loadMore button

---
+++ assets/js/frontoffice/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".card").slice(0, 15).show();
+    $("#loadMore").on("click", function (e) {
+        e.preventDefault();
+        $(".card:hidden").slice(0, 5).slideDown();
+        if ($(".card:hidden").length === 0) {
+            $("#loadMore").text("Aucun trick à afficher");
+        }
+    });
+})
