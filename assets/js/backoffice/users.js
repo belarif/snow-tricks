8565a87f9a25805722509034e64ff91d32bdd1e7
belarif <b.ocine@live.fr>
2022-02-16 10:16:27 +0100

    ST-main: ignoring files

--- assets/js/backoffice/users.js
+++
@@ -1,9 +0,0 @@
-function deleteUser(id) {
-    if (confirm("Êtes vous sûr de vouloir supprimer l'utilisateur ?")) {
-        window.location.href = "delete/" + id;
-    } else {
-        alert("Vous avez annulé la suppression");
-    }
-}
-
-window.deleteUser = deleteUser;
