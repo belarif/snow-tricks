62bfe623c3b6ce645c96bdb36999586c49bbdd03
belarif <b.ocine@live.fr>
2022-02-17 13:24:54 +0100

    ST-main: revert assets files

---
+++ assets/frontoffice.js
@@ -0,0 +1,24 @@
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+import('./styles/frontoffice/styles.css');
+import('./styles/frontoffice/home.css');
+import('./styles/frontoffice/trick_details.css');
+import('./styles/frontoffice/edit_trick.css');
+import('./js/frontoffice/home.js');
+import('./js/frontoffice/trick_details');
+import('./js/frontoffice/delete_trick');
+import('./js/frontoffice/edit_trick');
+import('./js/frontoffice/create_trick');
+import('./styles/frontoffice/register.css');
+import('./styles/frontoffice/register.css');
+import('./styles/frontoffice/login.css');
+import('./styles/frontoffice/create_trick.css');
+import('./styles/frontoffice/profile.css');
+import('./styles/frontoffice/forgot_password.css');
+import('./styles/frontoffice/reset_password.css');
+import('./styles/frontoffice/template.css');
+
