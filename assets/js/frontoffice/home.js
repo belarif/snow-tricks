77c709bb0d1148860d71599b484aade5a37f6f20
belarif <b.ocine@live.fr>
2022-02-17 15:22:30 +0100

    ST-main: changing by class card-trick

--- assets/js/frontoffice/home.js
+++ assets/js/frontoffice/home.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $(".card").slice(0, 15).show();
+    $(".card-trick").slice(0, 15).show();
     $("#loadMore").on("click", function (e) {
         e.preventDefault();
-        $(".card:hidden").slice(0, 5).slideDown();
-        if ($(".card:hidden").length === 0) {
+        $(".card-trick:hidden").slice(0, 5).slideDown();
+        if ($(".card-trick:hidden").length === 0) {
             $("#loadMore").text("Aucun trick");
         }
     });
