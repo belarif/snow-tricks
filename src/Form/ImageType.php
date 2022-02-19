88eb9843bd4f9a81ff20776261268c49d646717b
belarif <b.ocine@live.fr>
2022-02-19 08:04:20 +0100

    ST-34: customizing form_image form

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,20 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('src')
-            ->add('alt')
-            ->add('trick');
+            ->add('src', FileType::class, [
+                'label' => false,
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control form-control-sm',
+                    'id' => 'editImage'
+                ]
+            ])
+            ->add('envoyer', SubmitType::class, [
+                    'attr' => [
+                        'class' => 'btn btn-sm btn-outline-secondary',
+                        'id' => 'editImage'
+                    ]]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
