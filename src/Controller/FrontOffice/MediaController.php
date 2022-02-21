af1ffed6606c61fff59a2824903dd765a9addcc0
belarif <b.ocine@live.fr>
2022-02-21 11:04:31 +0100

    ST-40: correcting comparaison operator

--- src/Controller/FrontOffice/MediaController.php
+++ src/Controller/FrontOffice/MediaController.php
@@ -90,7 +90,7 @@ class MediaController extends AbstractController
             $trick_id = $image->getTrick()->getId();
 
             $src = $formImage->get('src')->getData();
-            if ($src == null) {
+            if ($src === null) {
                 return $this->redirectToRoute('trick_edit', ['id' => $trick_id, 'slug' => $slug]);
             }
 
