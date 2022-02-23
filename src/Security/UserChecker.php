7066adf493fe24ee2ac5ca90fd0c174331747e39
bocin <b.ocine@live.fr>
2022-02-23 16:41:43 +0100

    ST-46: creating user checker that verify if user account is activated or no

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if(!$user instanceof AppUser) {
+            return;
+        }
+
+        if ($user->getEnabled() !== true) {
+            throw new CustomUserMessageAccountStatusException('Votre compte est désactivé');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
+}
